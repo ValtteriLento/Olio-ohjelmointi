4bf916976629853b4e50b594fcf07c58d1058d65
ValtteriLento <valtteri.lento@gmail.com>
2023-01-25 16:42:04 +0200

    commit

---
+++ viikko2/src/main/Hedgehog.java
@@ -0,0 +1,31 @@
+package main;
+
+public class Hedgehog {
+    private String name;
+    private int age;
+
+    public Hedgehog() {
+        name = "Pikseli";
+        age = 5;
+    }
+
+    public Hedgehog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void speak(String says) {
+        if (says.isEmpty() == true) {
+            System.out.println("Olen " + name + " ja ikäni on " + age + ", mutta antaisitko silti syötteen?");
+        } else {
+            System.out.println(name + ": " + says);
+        }
+    }
+
+    public void run(int rounds) {
+        for (int i = 0; i < rounds; i++) {
+            System.out.println(name + " juoksee kovaa vauhtia!");
+        }
+    }
+    
+}
